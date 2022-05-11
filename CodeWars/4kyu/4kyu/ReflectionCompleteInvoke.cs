932d75675eb8d75cfa9cee701f029dac3775bbc6
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-05-11 17:54:14 +0200

    WorkWith Reflection4 Challenge

---
+++ CodeWars/4kyu/4kyu/ReflectionCompleteInvoke.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _4kyu
+{
+    internal class ReflectionCompleteInvoke
+    {
+        public string InvokeMethod(string typeName)
+        {
+            return "?";
+        }
+    }
+}
