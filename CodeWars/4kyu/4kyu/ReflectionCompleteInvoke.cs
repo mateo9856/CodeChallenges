42ab66086f6b92caf9a6690b6f6a862c89db7a69
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-05-22 15:18:49 +0200

    add comments to this code

--- CodeWars/4kyu/4kyu/ReflectionCompleteInvoke.cs
+++ CodeWars/4kyu/4kyu/ReflectionCompleteInvoke.cs
@@ -10,17 +10,19 @@ namespace _4kyu
         {
             try
             {
+                //get type 
                 var type = Type.GetType(typeName);
-
+                //check if value is not null or empty
                 if (string.IsNullOrEmpty(typeName))
                     return typeName;
 
+                //get methods where length parameter equal 0 and return type is string
                 return (string)type?.GetMethods()
                 .Where(d => d.ReturnType == typeof(string) && d.GetParameters().Length == 0).First()?
                     .Invoke(FormatterServices.GetUninitializedObject(type), null);
             }
             catch (ArgumentNullException)
-            {
+            {//if typeName as parameter is null and 
                 return null;
             }
         }
