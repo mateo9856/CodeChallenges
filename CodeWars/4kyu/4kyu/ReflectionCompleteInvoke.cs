75f4f53b9f1a56095806535a6d13778e1b54cb6d
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-05-22 15:16:37 +0200

    Add Reflection#4 code

--- CodeWars/4kyu/4kyu/ReflectionCompleteInvoke.cs
+++ CodeWars/4kyu/4kyu/ReflectionCompleteInvoke.cs
@@ -1,6 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Text;
+using System.Linq;
+using System.Runtime.Serialization;
 
 namespace _4kyu
 {
@@ -8,7 +8,21 @@ namespace _4kyu
     {
         public string InvokeMethod(string typeName)
         {
-            return "?";
+            try
+            {
+                var type = Type.GetType(typeName);
+
+                if (string.IsNullOrEmpty(typeName))
+                    return typeName;
+
+                return (string)type?.GetMethods()
+                .Where(d => d.ReturnType == typeof(string) && d.GetParameters().Length == 0).First()?
+                    .Invoke(FormatterServices.GetUninitializedObject(type), null);
+            }
+            catch (ArgumentNullException)
+            {
+                return null;
+            }
         }
     }
 }
