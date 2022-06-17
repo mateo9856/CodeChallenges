c0ee0bb77d4faa23a9747419a4bcce1d00bdcfde
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-06-17 11:11:25 +0200

    valid parentheses

---
+++ CodeWars/4kyu/5kyu/ValidParentheses.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace _5kyu
+{
+    public class ValidParentheses
+    {
+        public bool Valid(string input)
+        {
+            int token = 0;
+
+            for (int i = 0; i < input.Length && token >= 0; i++)
+            {
+                token += (input[i] == '(') ? 1 : (input[i] == ')') ? -1 : 0;
+            }
+
+            return token == 0;
+
+        }
+    }
+}
