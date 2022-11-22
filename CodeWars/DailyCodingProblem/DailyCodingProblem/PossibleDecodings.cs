4d7916e2dbd8e7f793038b2a026b900b9427e2f8
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-11-22 22:30:29 +0100

    problems

---
+++ CodeWars/DailyCodingProblem/DailyCodingProblem/PossibleDecodings.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    public class PossibleDecodings
+    {//przemodelować po swojemu!! ta metode
+        //https://www.geeksforgeeks.org/count-possible-decodings-given-digit-sequence/
+        public int countDecoding(char[] digits, int n)
+        {
+            if (n == 0 || n == 1)
+                return 1;
+
+            // Initialize count
+            int count = 0;
+
+            if (digits[n - 1] > '0')
+                count = countDecoding(digits, n - 1);
+
+            if (digits[n - 2] == '1'
+                || (digits[n - 2] == '2'
+                    && digits[n - 1] < '7'))
+                count += countDecoding(digits, n - 2);
+
+            return count;
+        }
+        public int countWays(char[] digits, int n)
+        {
+            if (n == 0 || (n == 1 && digits[0] == '0'))
+                return 0;
+            return countDecoding(digits, n);
+        }
+    }
+}
---
+++ CodeWars/DailyCodingProblem/DailyCodingProblem/UnivalTreeCount.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    public class UnivalTreeCount
+    {
+        public int UnivalCount(BinaryNode node, BinaryNode prevVal = null, int count = 0)
+        {
+            //analyze and patch
+            var l = node.Left;
+            var r = node.Right;
+
+            if(l != null) count += UnivalCount(l, node, count);
+            if(r != null) count += UnivalCount(r, node, count);
+
+            if (prevVal != null && prevVal.Data == node.Data) count++;
+            else if (prevVal.Data != node.Data && (prevVal.Left.Data == prevVal.Right.Data)) count += 2;
+
+            return count;
+        }
+
+    }
+
+    public class BinaryNode
+    {
+        public int Data { get; set; }
+        public BinaryNode Left { get; set; }
+        public BinaryNode Right { get; set; }
+    }
+
+}
