f9c38c2575604db8f3a038f41075410b17da861b
Mateusz <Mateusz@LAPTOP-EP5S1A81>
2022-11-26 02:30:33 +0100

    Job Scheduler

---
+++ CodeWars/DailyCodingProblem/DailyCodingProblem/JobScheduler.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DailyCodingProblem
+{
+    public class JobScheduler<T>
+    {
+        public delegate T ObjDelegate();
+        public void Schedule(int n, ObjDelegate f)
+        {
+            Task.Delay(n);
+
+            f.Invoke();
+        }
+    }
+}
--- CodeWars/DailyCodingProblem/DailyCodingProblem/Program.cs
+++ CodeWars/DailyCodingProblem/DailyCodingProblem/Program.cs
@@ -1,4 +1,10 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using DailyCodingProblem;
 
-Console.WriteLine(new TwoNumbers().IsAddedToK(new int[] {16, 15, 10, 7 }, 17));
+int[] arr = { 2, 4, 6, 2, 5 };
+var el = new NonAdjacentNumbers();
+
+var sched = new JobScheduler<int>();
+sched.Schedule(4000, () => el.Get(arr));
+
+Console.WriteLine(el);
